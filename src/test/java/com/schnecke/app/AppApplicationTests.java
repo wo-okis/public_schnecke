71be71c15ee99fc47066c5e3a7d50c01770d2f98
97Chanuk <chanuk9531@daum.net>
2024-08-13 18:00:45 +0900

    'helloworld'

---
+++ src/test/java/com/schnecke/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schnecke.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
